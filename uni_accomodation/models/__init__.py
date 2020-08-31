2beaf2deb4bac18d9b734091477afc6a470368b4
ope <adeyemiopeyemi190@yahoo.com>
2020-08-31 09:31:35 +0100

    Add more features to Hostel Management Module

---
+++ uni_accomodation/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import accomodation
+from . import room
+from . import block
+from . import portal
+from . import hostel
--- uni_accomodation/models/accomodation.py
+++ uni_accomodation/models/accomodation.py
@@ -1,43 +1,54 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+
 
 class saleOrderInherit(models.Model):
     _inherit = 'sale.order'
-    name= fields.Char(string='Name')
+    name = fields.Char(string='Name')
+
 
 class StudentAccomodation(models.Model):
-    _name= 'student.accomodation'
+    _name = 'student.accomodation'
     _inherit = ['mail.thread', 'mail.activity.mixin']
-    _description= 'Accomodation'
-
+    _description = 'Accomodation'
+    
     #fields
-    matric_no = fields.Char(string='Matric Number', required=True)
+    matric_no = fields.Char(string='Matric Number', required=True )
     name = fields.Char(string='Name')
-    image= fields.Binary(string="Image", related="")
-    gender = fields.Selection([
+    image = fields.Binary(string="Image", related="")
+    name_seq = fields.Char(string='Hostel Reference', required=True, copy=False, readonly=True, index=True, default=lambda self: _('New'))
+    faculty = fields.Many2one(
+        "uni.faculty", string="Faculty", required=True
+    )
+    date = fields.Date(string='Date')
+    hostel_name = fields.Many2one("hostel.name", string="Hostel Name"
+    )
+    hostel_type = fields.Selection([
          ("male", "Male"),
-         ("fe_male","Female"),
-         ], string="Gender", related="")
-
-    #fields for school accomodation
-    school_accomodation= fields.Selection([
-         ("Madam Tinubu hall", "Madam Tinubu hall"),
-         ("King Jaja", "King Jaja"),
-         ("Queen Amina Hall", "Queen Amina Hall"),
-         ("Princess Adeyemi Hall", "Princess Adeyemi Hall"),
-         ("Biobaku Hall", "Biobaku Hall"),
-         ],string="School Accomodation")
+         ("female", "Female"),
+         ], string="Gender", required=True)
+    room_selection = fields.Selection([
+          ("2", "2 Man Room"),
+          ("4", "4 Man Room"),
+          ("8", "8 Man room"),
+          ("s", "Reserved for Research Scholars"),
+          ("p", "Physically Chanllenged"),
+        ], string="Room Selection")
+    bunk = fields.Selection([
+        ("Up", "Up Bunk"),
+        ("Down", "Down Bunk"),
+    ], string="Bunk")
+    hostel_block = fields.Many2one("hostel.block", string="Block"
+    )
+    room = fields.Many2one("hostel.room", string="Room No"
+    )
+    hostel_portal = fields.Many2one("hostel.portal", string="Portal Details"
+    )
 
     @api.model
     def create(self, vals):
-             vals ['name'] = self.env['ir.sequence'].next_by_code('student.accomodation.sequence')
+             vals['name'] = self.env['ir.sequence'].next_by_code('student.accomodation.sequence')
              result = super(StudentAccomodation, self).create(vals)
              return result
-    
 
-
-    
-
-
-  
-  
-
+    def submit_allocate(self):
+             pass
