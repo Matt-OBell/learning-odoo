394f4ec67f11ab1164e395a0e7fd13d1d43bfd3c
Peter Ajiboye <peterajiboy@gmail.com>
2020-09-07 08:12:57 -0700

    added book view

--- uni_lms/models/library_book.py
+++ uni_lms/models/library_book.py
@@ -1,13 +1,21 @@
 from odoo import models, fields
 
 
+class Category(models.Model):
+    _name = 'category.library_book'
+    _description = 'Category'
+    name = fields.Char(string='Category')
+
+
 class Book(models.Model):
     _name = 'library.book'
     _description = 'Books'
-    name = fields.Char(string='Title', required=True) 
-    isbn = fields.Char(string='ISBN')
-    active = fields.Boolean('Active?', default=True)
-    published_date = fields.Date()
+    name = fields.Char(string='Title', required=True)
     image = fields.Binary()
-    publisher_id = fields.Many2one('res.partner', string='Publisher')
     author_ids = fields.Many2many('res.partner', string='Authors')
+    isbn = fields.Char(string='ISBN')
+    published_date = fields.Date()
+    publisher_id = fields.Many2one('res.partner', string='Publisher')
+    active = fields.Boolean('Active?', default=True)
+    #category = fields.Char()
+    category = fields.Many2many('category.library_book')
